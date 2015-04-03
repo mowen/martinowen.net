3b912b9385c8dc9db50037b12a33d1189a3ab868
Martin Owen <martinowenuk@gmail.com>
2015-04-03 08:37:42 +0100

    Minor changes to make everything work with Ruby 2.2.1.

--- lib/default.rb
+++ lib/default.rb
@@ -1,6 +1,2 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-
-# Use Syck as YAML engine, because Psych has issue with ultraviolet's syntax files
-require 'yaml'
-YAML::ENGINE.yamler = 'syck'
